1b79c25e9fe2d221bb8f542d5e50d4f1c54f0482
Turner Bohlen <turnerbohlen@gmail.com>
2012-07-23 20:46:18 +0000

    Modifies install.sh to move .gitconfig

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 #! /bin/bash
-#symlink .vimrc
-ln -s .vimrc ~/.vimrc
-#symlink .vim
-ln -s .vim ~/.vim
-#symlink the .dircolors
-ln -s .dircolors ~/.dircolors
+
+# find current directory
+DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# remove existing files
+rm "${HOME}/.vimrc"
+rm "${HOME}/.dircolors"
+rm -r "${HOME}/.vim"
+rm -r "${HOME}/.gitconfig"
+
+#symlink files into place
+ln -s "${DIR}/.vimrc" "${HOME}/.vimrc"
+ln -s "${DIR}/.vim" "${HOME}/.vim"
+ln -s "${DIR}/.dircolors" "${HOME}/.dircolors"
+ln -s "${DIR}/.gitconfig" "${HOME}/.gitconfig"
